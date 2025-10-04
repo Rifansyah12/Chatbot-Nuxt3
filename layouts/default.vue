edc0322d5477128238381f5bda48c61d77604bac
RIFANSYAH <syahr9610@gmail.com>
2025-10-05 03:31:03 +0700

    Prepare Nuxt 3 for Netlify deployment

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <div class="min-h-screen bg-gray-100 text-gray-900">
     <header class="p-4 bg-gray-900 text-white">
       <h1 class="text-xl font-bold">🚀 Chatbot App</h1>
@@ -7,4 +7,4 @@
       <slot />
     </main>
   </div>
-</template> -->
+</template>
